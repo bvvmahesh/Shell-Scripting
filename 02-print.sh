aa91473f9176153b901426f648f918663e3ffbae
bvvmahesh <bavisetty.veera@gmail.com>
2020-04-05 16:10:37 +0530

    added

--- 02-print.sh
+++ 02-print.sh
@@ -6,6 +6,6 @@
 
 # we choose to go with echo as it reduces the syntax  problems unlike printf
 
-echo This is Mahesh working as a Devops Engineer
+echo This is Mahesh working as a Devops Engineer. Hello world
 echo Basics of shell scripting.
 echo Hello world
